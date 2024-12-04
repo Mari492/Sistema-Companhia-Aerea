d2f3e76acd17ec9fa4109c2c60b43fe4c1893aa7
TIAGO BENEVENUTO <tabenevenuto@sga.pucminas.br>
2024-12-04 20:36:56 -0300

    assento.h

--- include/assento.h
+++ include/assento.h
@@ -1,37 +1,22 @@
 #ifndef ASSENTO_H
 #define ASSENTO_H
 
-#include <vector>
-#include <string>
-
 using namespace std;
 
 class Assento {
 private:
-    int codigoVoo; // Código do voo ao qual o assento pertence
-    int numeroAssento; // Número do assento (1 a 60)
-    bool status; // Status do assento (livre ou ocupado)
-    
+    int numero;
+    int codigoVoo;
+    bool ocupado;
+
 public:
-    Assento(int codigoVoo, int numeroAssento);
+    Assento();
+    Assento(int num, int codVoo);
 
-    // Métodos para acessar os dados do assento
-    int getCodigoVoo() const;
-    int getNumeroAssento() const;
-    bool getStatus() const;
-
-    // Método para reservar o assento
-    bool reservarAssento();
-
-    // Método estático para inicializar os assentos de um voo
-    static void inicializarAssentos(int codigoVoo);
-
-    // Método estático para buscar um assento
-    static Assento buscarAssento(int codigoVoo, int numeroAssento);
-
-    // Método estático para exibir todos os assentos de um voo
-    static void listarAssentos(int codigoVoo);
+    int getNumero() const;
+    bool estaOcupado() const;
+    void ocupar();
+    void liberar();
 };
 
 #endif
-
